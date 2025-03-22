8782c4bc64e7dcb9f7721b8443cd821a779c42ba
Sujin <140675277+cckim-bamba@users.noreply.github.com>
2025-03-22 14:02:33 +0000

    질문 업데이트, mvp4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎬 Movie Script Test3")
+st.title("🎬 Movie Script Test4")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
